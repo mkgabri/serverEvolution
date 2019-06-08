845b53ac640cee235d1e05911ebc332f9a3e2fd9
gabriel carrasco <gabriel136@hotmail.it>
2019-06-08 11:01:22 +0200

    inserrimento codice HTML
    prova d'inserimento di codice dentro la risposta

--- server.js
+++ server.js
@@ -2,7 +2,18 @@ var http = require('http');
 
 var server = http.createServer(function(req, res) {
     res.writeHead(200, {"Content-Type": "text/html"});
+    res.write('<!DOCTYPE html>'+
+      '<html>'+
+      ' <head>'+
+      ' <meta charset="utf-8" />'+
+      ' <title>Pagina Node.js!</title>'+
+      ' </head>'+
+      ' <body>'+
+      '  <p>Sono un paragrafo <strong>HTML</strong>!</p>'+
+      ' </body>'+
+      '</html>');
+    res.end();
 //    res.end('Ciao a tutti, sono un web server!!!!!');
-    res.end('<p>Io sono un paragrafo <strong>HTML</strong></p>');
+//    res.end('<p>Io sono un paragrafo <strong>HTML</strong></p>');
 });
 server.listen(8080);
