eaf37c2c128a7a6739b34648853db3922966355a
gabriel carrasco <gabriel136@hotmail.it>
2019-06-08 12:04:49 +0200

    analize response name in get

--- server.js
+++ server.js
@@ -1,19 +1,15 @@
 var http = require('http');
+var url = require("url");
+var querystring = require('querystring')
 
 var server = http.createServer(function(req, res) {
+    var params = querystring.parse(url.parse(req.url).query);
     res.writeHead(200, {"Content-Type": "text/html"});
-    res.write('<!DOCTYPE html>'+
-      '<html>'+
-      ' <head>'+
-      ' <meta charset="utf-8" />'+
-      ' <title>Pagina Node.js!</title>'+
-      ' </head>'+
-      ' <body>'+
-      '  <p>Sono un paragrafo <strong>HTML</strong>!</p>'+
-      ' </body>'+
-      '</html>');
+    if ('firstname' in params && 'lastname' in params) {
+        res.write('Il tuo nome: ' + params['firstname'] + ' ' + params['lastname']);
+    } else {
+        res.write('Tu hai un nome giusto?');
+    }
     res.end();
-//    res.end('Ciao a tutti, sono un web server!!!!!');
-//    res.end('<p>Io sono un paragrafo <strong>HTML</strong></p>');
 });
 server.listen(8080);
